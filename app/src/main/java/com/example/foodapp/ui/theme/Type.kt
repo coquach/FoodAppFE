5f4f6108ddd8d573ac96e4ee1f289aa9c77542f9
vinhco1 <23520189@gm.uit.edu.vn>
2025-03-15 21:58:28 +0700

    configure sora font

--- app/src/main/java/com/example/foodapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/foodapp/ui/theme/Type.kt
@@ -2,33 +2,39 @@ package com.example.foodapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.foodapp.R
 
 // Set of Material typography styles to start with
+val displayFontFamily = FontFamily(
+    Font(R.font.sora_regular, FontWeight.Normal),
+    Font(R.font.sora_medium, FontWeight.Medium),
+    Font(R.font.sora_semibold, FontWeight.SemiBold),
+    Font(R.font.sora_bold, FontWeight.Bold),
+    Font(R.font.sora_thin, FontWeight.Thin),
+    Font(R.font.sora_light, FontWeight.Light),
+    Font(R.font.sora_extralight, FontWeight.ExtraLight),
+    Font(R.font.sora_extrabold, FontWeight.Black)
+)
+val baseline = Typography()
+// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = displayFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = displayFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = displayFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = displayFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = displayFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = displayFontFamily),
 )
